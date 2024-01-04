cadf2f555e03df6bbba84fd7a5dbddb85b4f291d
Jed Colina <jed.colina@fpt.com>
2024-01-04 11:02:13 -0800

    changed the link name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacting!tae
+          Learn React!
         </a>
       </header>
     </div>
